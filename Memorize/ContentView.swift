b3c5115ff244ffe13b9a259c2d62439dbc37fb9d
Alex Zdanowicz <Zandwhich@users.noreply.github.com>
2020-06-28 16:21:25 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by First Last on 2020-06-28.
+//  Copyright © 2020 Alex Zdanowicz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
